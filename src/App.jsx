0b6c58add74686a36eaa29bb83093ec9e5594556
oogvay1 <atalgat592@gmail.com>
2025-05-28 15:34:49 +0500

    Json-Server

---
+++ src/App.jsx
@@ -0,0 +1,177 @@
+import { useCallback, useState, useEffect } from 'react'
+import './App.css'
+import Loader from './components/Loader/Loader';
+import Modal from './components/Modal/Modal';
+
+function App() {
+
+  let [url, setUrl] = useState('http://localhost:8080/Users');
+  let [data, setData] = useState(null);
+  let [error, setError] = useState(false);
+  let [load, setLoad] = useState(false);
+  let [editTitle, setEditTitle] = useState('');
+  let [editingId, setEditingId] = useState(null);
+  let [apiUrl, setApiUrl] = useState('http://localhost:8080/Users');
+  let [esc, setEsc] = useState(false);
+
+  let [form, setForm] = useState({
+    name: '',
+    lastname: '',
+    age: '',
+    gender: '',
+    country: '',
+    isDeveloper: false,
+  });
+
+  let newUserr = {
+    name: '',
+    lastname: '',
+    age: '',
+    gender: '',
+    country: '',
+    isDeveloper: false,
+  }
+
+  const handleForm = (e) => {
+    const { name, value, type } = e.target;
+    setForm(prev => ({
+      ...prev,
+      [name]: type === "number" ? + value : value
+    }))
+  };
+
+  const fetchData = useCallback(async () => {
+    setError(false);
+    setLoad(true);
+
+    try {
+      const response = await fetch(url)
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`)
+      }
+      const json = await response.json()
+      setData(json);
+    } catch (error) {
+      setError(true);
+    } finally {
+      setLoad(false);
+    }
+  }, [url]);
+
+  useEffect(() => {
+    fetchData()
+  }, [fetchData])
+
+  const objectEmpty = Object.values(form).every(value => {
+    if (typeof value === 'string') {
+      return value.trim() === '';
+    }
+
+    return !value;
+  });
+
+  const addUser = async () => {
+    setEsc(true);
+    setEsc(false)
+    if (objectEmpty) return;
+
+    const res = await fetch(url, {
+      method: "POST",
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ name: form.name, lastname: form.lastname, age: form.age, country: form.country, isDeveloper: form.isDeveloper })
+    });
+    const user = await res.json();
+
+    setData(prev => [...prev, user]);
+    setForm(newUserr);
+  }
+
+  const deleteUser = async (id) => {
+    await fetch(`${url}/${id}`, { method: "DELETE" });
+    setData(prev => prev.filter(user => user.id !== id));
+  }
+
+  const updateUser = async (id) => {
+    const res = await fetch(`${url}/${id}`, {
+      method: 'PUT',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ name: editTitle })
+    });
+    const update = await res.json();
+    setData(prev => prev.map(user => user.id === id ? update : user));
+
+    setEditTitle('');
+    setEditingId(null);
+  }
+
+  const handleChange = (newUrl) => {
+    setUrl(newUrl);
+  }
+
+  if (load) {
+    return <Loader />
+  }
+
+  if (error) {
+    return <p>Error: {error.message}</p>
+  }
+
+
+  return (
+    <>
+      {esc && <Modal form={form} setEsc={() => setEsc(false)} addUser={addUser} handle={handleForm} />}
+
+      <div className="container">
+
+
+        <div className="main-header">
+          <div className="change-url">
+            <h1>Change Api:</h1>
+            <input className='change-input' type="text" placeholder='New Url' value={apiUrl} onChange={(e) => setApiUrl(e.target.value)} />
+            <button onClick={() => handleChange(apiUrl)}>Change</button>
+          </div>
+          <div className="add-user">
+            <h1>Add user:</h1>
+            <button onClick={() => setEsc(true)}>Add new User</button>
+          </div>
+        </div>
+
+        <div className="users-list">
+          <h1>Users: {data ? data.length : 0}</h1> {
+            data && (
+              <ul>
+                {
+                  data.map(user => {
+                    return (
+                      <li key={user.id}>
+                        {editingId === user.id ? (
+                          <>
+                            <input type="text" value={editTitle} onChange={(e) => setEditTitle(e.target.value)} />
+                            <button onClick={() => updateUser(user.id)}>Save</button>
+                            <button onClick={() => setEditingId(null)}>Cancel</button>
+                          </>) : (
+                          <>
+                            {user.name ? user.name : user.title}
+                            <button onClick={() => {
+                              setEditingId(user.id);
+                              setEditTitle(user.title);
+                            }}>Edit</button>
+                            <button onClick={() => deleteUser(user.id)}>Delete</button>
+                          </>
+                        )}
+                      </li>
+                    )
+                  })
+                }
+              </ul>
+            )
+          }
+        </div>
+
+      </div>
+
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,80 @@
+import { useEffect } from 'react';
+import './Modal.css'
+
+function Modal({ form, addUser, handle, setEsc }) {
+
+    useEffect(() => {
+        const modalClose = (e) => {
+            if (e.key == "Escape") {
+                setEsc();
+            }
+        }
+
+        window.addEventListener('keydown', modalClose)
+
+        return () => {
+            window.removeEventListener('keydown', modalClose)
+        }
+    }, [setEsc])
+
+    return (
+        <div className="modal">
+            <div className="Modal">
+                <label>
+                    <h1>Name:</h1>
+                    <input type="text" name="name" placeholder="Name.." value={form.name} onChange={handle} autoComplete='off' />
+                </label>
+                <label>
+                    <h1>Lastname:</h1>
+                    <input type="text" name="lastname" placeholder="Lastname" value={form.lastname} onChange={handle} autoComplete='off' />
+                </label>
+                <label>
+                    <h1>Age:</h1>
+                    <input type="number" name="age" placeholder="Age" value={form.age} onChange={handle} autoComplete='off' />
+                </label>
+                <div className="gender">
+                    <h1>Gender:</h1>
+
+                    <div className="radio">
+                        <div>
+                            <label>
+                                <h1>Male</h1>
+                                <input type="radio" name="gender" checked={form.gender === 'male'} value={"male"} onChange={handle} />
+                            </label>
+                        </div>
+                        <div>
+                            <label>
+                            <h1>Female</h1>
+                                <input type="radio" name="gender" checked={form.gender === 'female'} value={"female"} onChange={handle} />
+                            </label>
+                        </div>
+                    </div>
+                </div>
+                <label>
+                    <h1>Country:</h1>
+                    <input type="text" name="country" placeholder="Country" value={form.country} onChange={handle} autoComplete='off' />
+                </label>
+                <label>
+                    <h1>Is Developer:</h1>
+                    <div className="radio">
+                        <div>
+                            <label>
+                                <h1>True</h1>
+                                <input type="radio" name="isDeveloper" checked={form.isDeveloper === 'true'} value={"true"} onChange={handle} />
+                            </label>
+                        </div>
+                        <div>
+                            <label>
+                                <h1>False</h1>
+                                <input type="radio" name="isDeveloper" checked={form.isDeveloper === 'false'} value={"false"} onChange={handle} />
+                            </label>
+                        </div>
+                    </div>
+                </label>
+                <button onClick={addUser}>Add</button>
+            </div>
+        </div>
+    );
+}
+
+export default Modal;
